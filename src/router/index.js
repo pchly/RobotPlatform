57fe2dd4286538e016660d13cfd3c9795bf82e57
Peng chunhao <1650211040@qq.com>
2021-04-16 15:39:07 +0800

    adjust the contents add the pages to pages contents

--- src/router/index.js
+++ src/router/index.js
@@ -14,56 +14,56 @@ const routes = [
       {
         path:'/home',
         name:'home',
-        component:() => import("../components/Home.vue"),
+        component:() => import("../pages/Home.vue"),
         props:true
       },
       //基础控制
       {
         path:'/basicControl',
         name:'basicControl',
-        component:() => import("../components/BasicControl.vue"),
+        component:() => import("../pages/BasicControl.vue"),
         props:true
       },
       //blockly
       {
         path:'/blockly',
         name:'blockly',
-        component:() => import("../components/Blockly.vue"),
+        component:() => import("../pages/Blockly.vue"),
         props:true
       },
       //python
       {
         path:'/python',
         name:'python',
-        component:() => import("../components/Python.vue"),
+        component:() => import("../pages/Python.vue"),
         props:true
       },
       //vision
       {
         path:'/vision',
         name:'vision',
-        component:() => import("../components/Vision.vue"),
+        component:() => import("../pages/Vision.vue"),
         props:true
       },
       //消息测试,WebSocket
       {
         path:'/webSocketTest',
         name:'webSocketTest',
-        component:() => import("../components/WebSocketTest.vue"),
+        component:() => import("../pages/WebSocketTest.vue"),
         props:true
       },
       //设置页面
       {
         path:'/setting',
         name:'setting',
-        component:() => import("../components/setting.vue"),
+        component:() => import("../pages/setting.vue"),
         props:true
       },
       //预留页面
       {
         path:'/reserve',
         name:'reserve',
-        component:() => import("../components/reserve.vue"),
+        component:() => import("../pages/reserve.vue"),
         props:true
       }
       // //ElementUi测试
