f361defe31ae14cd5e0ba50eaabc948b5b053249
Peng chunhao <1650211040@qq.com>
2021-05-14 16:46:47 +0800

    PCH-2021-05-14在reserve页面增加webgl

--- src/pages/reserve.vue
+++ src/pages/reserve.vue
@@ -3,12 +3,30 @@
 		  <el-button @click="backToHome" type="primary" icon="el-icon-arrow-left">返回</el-button>
 		</el-button-group>
     <hr />
-        <img src="../assets/reserve.png" style="width:100%" alt="">
+     <canvas id="myCa"></canvas>
+     <img src="../assets/reserve.png" style="width:100%" alt="">
     </div>
 </template>
 <script>
 export default {
     name:"reserve",
+    data:function(){
+        return{
+          currentStep:1,
+          clickedStep:1,
+          sendMessage:'',
+          receivedMessage:''
+        }
+    },
+    mounted(){
+      console.log('123');
+      //获取上下文对象
+      const canvas = document.querySelector("#myCa");
+      var webGL = canvas.getContext("webgl");
+      webGL.viewport(0,0,600,800);
+      webGL.clearColor(0.0,1.0,0.0,0.5);
+      webGL.clear(webGL.COLOR_BUFFER_BIT);
+    },
     methods:{
       backToHome(){
         this.$router.push('/home');
