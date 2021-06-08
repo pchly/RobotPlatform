b102a2529a0c96f627b0af04d01e8233237b8079
Peng chunhao <1650211040@qq.com>
2021-06-08 21:32:55 +0800

    PCH-2021-06-08完成设置界面

--- src/store/index.js
+++ src/store/index.js
@@ -45,9 +45,9 @@ const store=new Vuex.Store({
             "XPos":'0.0',
             "YPos":'0.0',
             "ZPos":'0.0',
-            "Roll":'0.0',
             "Pitch":'0.0',
             "Yaw":'0.0',
+            "Roll":'0.0'
           },
           positionOfAxisInReal:{
             "oneAxis":'0.0',
@@ -60,13 +60,122 @@ const store=new Vuex.Store({
           },
           enableRobot:false,
           moveVecReal:0.0,
-          kindOfEndTool:0,//0代表夹爪，1代表吸盘
+          kindOfEndTool:2,//0代表夹爪，1代表吸盘,2代表无
           stateOfEndSuck:false,
           posOfEndJaw:0.0,
           theRunInformation:'未连接机械臂',
+          runInformationDataBuff:[],
           realRobotControlMode:1,//1代表点动模式,2代表手动模式
           outExeclDataSimulate:[],
           outExeclDataHandMode:[],
+
+
+          AxisOriginSettingData:{
+            "oneAxis":'0.0',
+            "twoAxis":'0.0',
+            "threeAxis":'0.0',
+            "fourAxis":'0.0',
+            "fiveAxis":'0.0',
+            "sixAxis":'0.0',
+            "senveAxis":'0.0'
+          },
+          lineMoveSettingData:{
+            "speedLimit":'0.0',
+            "accLimit":'0.0',
+            "jerkLimit":'0.0'
+          },
+          oneAxisSettingData:{
+            "negativeLimitPosition":'0.0',
+            "positiveLimitPosition":'0.0',
+            "speedLimit":'0.0',
+            "accLimit":'0.0',
+            "jerkLimit":'0.0'
+          },
+          twoAxisSettingData:{
+            "negativeLimitPosition":'0.0',
+            "positiveLimitPosition":'0.0',
+            "speedLimit":'0.0',
+            "accLimit":'0.0',
+            "jerkLimit":'0.0'
+          },
+          threeAxisSettingData:{
+            "negativeLimitPosition":'0.0',
+            "positiveLimitPosition":'0.0',
+            "speedLimit":'0.0',
+            "accLimit":'0.0',
+            "jerkLimit":'0.0'
+          },
+          fourAxisSettingData:{
+            "negativeLimitPosition":'0.0',
+            "positiveLimitPosition":'0.0',
+            "speedLimit":'0.0',
+            "accLimit":'0.0',
+            "jerkLimit":'0.0'
+          },
+          fiveAxisSettingData:{
+            "negativeLimitPosition":'0.0',
+            "positiveLimitPosition":'0.0',
+            "speedLimit":'0.0',
+            "accLimit":'0.0',
+            "jerkLimit":'0.0'
+          },
+          sixAxisSettingData:{
+            "negativeLimitPosition":'0.0',
+            "positiveLimitPosition":'0.0',
+            "speedLimit":'0.0',
+            "accLimit":'0.0',
+            "jerkLimit":'0.0'
+          },
+          senveAxisSettingData:{
+            "negativeLimitPosition":'0.0',
+            "positiveLimitPosition":'0.0',
+            "speedLimit":'0.0',
+            "accLimit":'0.0',
+            "jerkLimit":'0.0'
+          },
+          XAxisSettingData:{
+            "negativeLimitPosition":'0.0',
+            "positiveLimitPosition":'0.0',
+            "speedLimit":'0.0',
+            "accLimit":'0.0',
+            "jerkLimit":'0.0'
+          },
+          YAxisSettingData:{
+            "negativeLimitPosition":'0.0',
+            "positiveLimitPosition":'0.0',
+            "speedLimit":'0.0',
+            "accLimit":'0.0',
+            "jerkLimit":'0.0'
+          },
+          ZAxisSettingData:{
+            "negativeLimitPosition":'0.0',
+            "positiveLimitPosition":'0.0',
+            "speedLimit":'0.0',
+            "accLimit":'0.0',
+            "jerkLimit":'0.0'
+          },
+          RollAxisSettingData:{
+            "negativeLimitPosition":'0.0',
+            "positiveLimitPosition":'0.0',
+            "speedLimit":'0.0',
+            "accLimit":'0.0',
+            "jerkLimit":'0.0'
+          },
+          PitchAxisSettingData:{
+            "negativeLimitPosition":'0.0',
+            "positiveLimitPosition":'0.0',
+            "speedLimit":'0.0',
+            "accLimit":'0.0',
+            "jerkLimit":'0.0'
+          },
+          YawAxisSettingData:{
+            "negativeLimitPosition":'0.0',
+            "positiveLimitPosition":'0.0',
+            "speedLimit":'0.0',
+            "accLimit":'0.0',
+            "jerkLimit":'0.0'
+          },
+
           // 自动上报的数据
           autoUpdateMessage:{
             // 机器人状态初始化为全零数组
